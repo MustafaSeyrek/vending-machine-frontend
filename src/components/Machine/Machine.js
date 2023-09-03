18513f7d3d4af515d6efcd4ca1766bc0852446c7
Mustafa Seyrek <seyrekmustafa42@gmail.com>
2023-09-03 18:48:52 +0300

    designed machine

--- src/components/Machine/Machine.js
+++ src/components/Machine/Machine.js
@@ -1,64 +1,116 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import axios from "axios";
 import "./Machine.css";
 export default function Machine() {
-  // const c=[]
-  // for(let i=0 ;i<2;i++){
-  //   c.push(<img className="image" src="/images/water.png" alt="water"></img>);
-  // }
-  // const p = ()=>{
-  //   return(<img className="image" src="/images/water.png" alt="water"></img>);
-  // }
-
-  // const designProduct = (product)=>{
-
-  // }
   const [products, setProducts] = useState([]);
   const [machine, setMachine] = useState([]);
+  const [error, setError] = useState(null);
+  const [selectedProduct, setSelectedProduct] = useState(0);
+  const [totalEntered, setTotalEntered] = useState([]); //keep total money
+  const [entered, setEntered] = useState(0);
+  const [total, setTotal] = useState(0);
+  const [change, setChange] = useState(0);
+  var productImgArr = []; //for dynamic product images
+  let accepted = ["1", "5", "10", "20"];
+  const inputRef = useRef(null);
 
   useEffect(() => {
     getProducts();
     getMachine();
   }, []);
 
-  const getProducts = async() => {
+  //axios requests from backend
+  const getProducts = async () => {
     var result = await axios.get("http://localhost:8080/api/products");
     setProducts(result.data);
   };
 
-  const getMachine = async() => {
-    var result = await axios.get("http://localhost:8080/api/machine/1");
-    setMachine(result.data);
-  }
+  const getMachine = async () => {
+    try {
+      var result = await axios.get("http://localhost:8080/api/machine/1");
+      setMachine(result.data);
+    } catch (e) {
+      setError(e.response.data);
+    }
+  };
+
+  //for selected product radio button event
+  const handleChange = (event) => {
+    setSelectedProduct(event.target.value);
+  };
+
+  //keep entered money
+  const handleInput = (value) => {
+    setEntered(value);
+  };
+
+  //for dynamic product images
+  const imgProduct = (product) => {
+    productImgArr = [];
+    let stock = product.stock;
+    //If there is stock >= 5, 5 items will be displayed on the screen.
+    if (stock >= 5) stock = 5;
+    else if (stock < 1) stock = 0;
+    for (let i = 0; i < stock; i++) {
+      productImgArr.push(
+        <img
+          className="image"
+          src={"/images/" + product.name.toLowerCase() + ".png"}
+          alt={product.name.toLowerCase()}
+        ></img>
+      );
+    }
+    return productImgArr;
+  };
+
+  //add entered money
+  const addMoneyClick = () => {
+    if (accepted.includes(entered)) {
+      //money control
+      setTotalEntered((current) => [...current, entered]);
+      setError(null);
+      inputRef.current.value = null;
+      setTotal(+total + +entered); //for sum with string
+    } else {
+      setError("Please enter 1, 5, 10 or 20 unit money!");
+    }
+    setEntered(0);
+  };
+
+  //cancel process
+  const handleCancel = () => {
+    setChange(total);
+    inputRef.current.value = null;
+    setTotalEntered([]);
+    if (total > 0) setError("Your money of " + total + "₺ has been refunded!");
+    setTotal(0);
+    setEntered(0);
+  };
+
+  //order process
+  const handleOrder = () => {
+    if (selectedProduct != "0") {
+      setError(null);
+    } else {
+      setError("Please select a product!");
+    }
+  };
 
   return (
     <div>
       <div className="machine">
         <div className="items">
-          <div className="shelf" id="water">
-            <img className="image" src="/images/water.png" alt="water"></img>
-            <img className="image" src="/images/water.png" alt="water"></img>
-            <img className="image" src="/images/water.png" alt="water"></img>
-            <img className="image" src="/images/water.png" alt="water"></img>
-            <img className="image" src="/images/water.png" alt="water"></img>
-          </div>
-          <div className="shelf" id="coke">
-            <img className="image" src="/images/coke.png" alt="coke"></img>
-            <img className="image" src="/images/coke.png" alt="coke"></img>
-            <img className="image" src="/images/coke.png" alt="coke"></img>
-            <img className="image" src="/images/coke.png" alt="coke"></img>
-            <img className="image" src="/images/coke.png" alt="coke"></img>
-          </div>
-          <div className="shelf" id="soda">
-            <img className="image" src="/images/soda.png" alt="soda"></img>
-            <img className="image" src="/images/soda.png" alt="soda"></img>
-            <img className="image" src="/images/soda.png" alt="soda"></img>
-            <img className="image" src="/images/soda.png" alt="soda"></img>
-            <img className="image" src="/images/soda.png" alt="soda"></img>
-          </div>
+          {products.map((product, index) => (
+            <div className="shelf" id={product.name.toLowerCase()}>
+              {imgProduct(product)}
+            </div>
+          ))}
         </div>
 
         <div className="order">
+          <div className="alert alert-danger" role="alert">
+            {error}
+          </div>
           <div className="container">
             <div className="process">
               <input
@@ -66,13 +118,26 @@ export default function Machine() {
                 className="priceEnter"
                 id="priceInput"
                 min="0"
+                onChange={(i) => handleInput(i.target.value)}
+                ref={inputRef}
               ></input>
-              <button className="addBtn">Add</button>
+              <button onClick={() => addMoneyClick()} className="addBtn">
+                Add
+              </button>
             </div>
+            <div className="totalEntered">
+              <label>Total: {total}₺</label>
+            </div>
+
             <div className="buttons">
-              <button className="orderBtn">Order</button>
-              <button className="cancelBtn">Cancel</button>
+              <button onClick={handleOrder} className="orderBtn">
+                Order
+              </button>
+              <button onClick={handleCancel} className="cancelBtn">
+                Cancel
+              </button>
             </div>
+
             <div className="selectProducts">
               {products.map((product, index) => (
                 <div className="form-check">
@@ -80,10 +145,17 @@ export default function Machine() {
                     className="form-check-input"
                     type="radio"
                     name="productRadio"
-                    id={product.name}
+                    id={product.id}
+                    disabled={product.stock < 1 ? true : false}
+                    onChange={handleChange}
+                    value={product.id}
                   />
-                  <label className="form-check-label" htmlFor={product.name}>
-                    {product.name}<small> {product.price}₺</small>
+                  <label className="form-check-label" htmlFor={product.id}>
+                    {product.name}
+                    <small>
+                      {" "}
+                      {product.stock < 1 ? "No stock" : product.price + "₺"}
+                    </small>
                   </label>
                 </div>
               ))}
